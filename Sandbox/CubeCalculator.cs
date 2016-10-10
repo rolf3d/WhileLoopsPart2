27730fcc6ec5698fceb2cc73473f1570de75c085
Martin Kierkegaard <mark@easj.dk>
2016-10-10 22:58:15 +0200

    Add project files.

---
+++ Sandbox/CubeCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class CubeCalculator
+    {
+        // Reads the users input from the console window
+        public String GetUserInput(String helpText)
+        {
+            Console.Write(helpText + " ");
+            String userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        // Checks if the given string can be converted into an integer
+        public bool IsANumber(String userInput)
+        {
+            try
+            {
+                Int32.Parse(userInput);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        // Converts the given string into an integer
+        public int ConvertToNumber(String userInput)
+        {
+            return Int32.Parse(userInput);
+        }
+
+        // Calculates the cube of the given integer
+        public int CalculateCube(int value)
+        {
+            return (value * value * value);
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            CubeCalculator cc = new CubeCalculator();
+
+            String userInput = cc.GetUserInput("Please enter an integer number:");
+            if (cc.IsANumber(userInput))
+            {
+                int value = cc.ConvertToNumber(userInput);
+                int result = cc.CalculateCube(value);
+                Console.WriteLine("The cube of {0} is {1}", value, result);
+            }
+            else
+            {
+                Console.WriteLine("Could not calculate...");
+            }
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
