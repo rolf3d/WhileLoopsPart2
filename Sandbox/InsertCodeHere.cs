4d4112a43e096ffc180c5ea0f61d27470040fa8f
Rolf Petersen <rollep7@msn.com>
2016-10-13 12:26:06 +0200

    userInput = cc.GetUserInput("Please enter an integer number:"); i bunden af while loop.

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,20 +7,31 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            
+                CubeCalculator cc = new CubeCalculator();
+   
 
-            CubeCalculator cc = new CubeCalculator();
+                String userInput = cc.GetUserInput("Please enter an integer number:");
 
-            String userInput = cc.GetUserInput("Please enter an integer number:");
-            if (cc.IsANumber(userInput))
-            {
-                int value = cc.ConvertToNumber(userInput);
-                int result = cc.CalculateCube(value);
-                Console.WriteLine("The cube of {0} is {1}", value, result);
+           
+                while (userInput != "q")
+                {
+                    if (cc.IsANumber(userInput))
+                    {
+                        int value = cc.ConvertToNumber(userInput);
+                        int result = cc.CalculateCube(value);
+                        Console.WriteLine("The cube of {0} is {1}", value, result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not calculate...");
+                    }
+                userInput = cc.GetUserInput("Please enter an integer number:");
             }
-            else
-            {
-                Console.WriteLine("Could not calculate...");
-            }
+
+               
+            
+            
 
             // The LAST line of code should be ABOVE this line
         }
